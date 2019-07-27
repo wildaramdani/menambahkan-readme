00241c4fdf9588489ab565a12f1a6d8af0df1ef7
wildaramdani <wildanramdani2017@gmail.com>
2019-07-27 16:48:13 +0700

    Latihanjs

---
+++ script.js
@@ -0,0 +1,80 @@
+console.log(" I am Learning!")
+
+document.getElementById("headtree").innerHTML= "Belajar Tekad Js!!";
+
+var name= "wildan";
+console.log(name);
+
+var lastName= "Ramdani"
+console.log(lastName);
+
+// CARA LAMA ES5
+
+var fullName = name + " " + lastName;
+
+console.log(name + " " + lastName);
+console.log("Nama saya: " + fullName);
+
+//CARA BARU ES6 
+console.log(`My Name is ${fullName}`);
+
+// komen 1 line
+/**
+ * line banyak
+ * aslina
+ */
+// Operator plus
+ var x =5;
+ var y =7;
+ var z = x +y;
+ console.log(z);
+
+//Operator Perkalian
+var a = 5;
+var b = 10;
+var c = a * b;
+console.log(c);
+
+//Operator
+/**
+ *  ++ increment
+ * -- decrement
+ * % modulo
+*/
+
+ var d = 10;
+ d += 1;
+ console.log(d);
+
+ var d = 10;
+ d *= 1;
+ console.log(d);
+
+ var d = 10;
+ d -= 1;
+ console.log(d);
+
+ var d = 10;
+ e = d % 3;
+ console.log(2);
+
+ var txt1 = "Persib";
+ var txt2 = "Bandung";
+ var txt3 = txt1 + " " + txt2;
+ console.log(txt3);
+
+ /**
+  * ==  : equal valeu
+  * === : equal value * type
+  * !=  : not equal
+  * !== : not equal value * type
+  *  >  : lebih besar
+  *  <  : lebih kesil
+  * =>  : lebih besar +
+  * ?   : ternary
+  */
+
+
+
+
+
